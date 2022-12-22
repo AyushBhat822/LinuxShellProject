15d58dfe20fab9500f707b34b8b1dbab59ef2181
Ayush Bhat <67255324+AyushBhat822@users.noreply.github.com>
2022-12-22 20:14:19 +0530

    Update and rename backup.sh to Backup.sh

--- Backup.sh
+++ Backup.sh
@@ -5,13 +5,13 @@ echo "Enter Source Directory"
 
 read src_dir
 
-echo "Enter Target Directory
+echo "Enter Target Directory"
 
 read tgt_dir
 
 #tgt_dir=/root/backups
 
-curr_timestamp=$(date ''+%Y-%m-%d-%H-%M-%S")
+curr_timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
 backup_file=$tgt_dir/$curr_timestamp.tgz
 
 echo "Taking backup on $curr_timestamp"
