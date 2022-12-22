6ab3b5f6255f17ea2d110304f17cfaecd468843c
Ayush Bhat <67255324+AyushBhat822@users.noreply.github.com>
2022-12-22 21:30:33 +0530

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -3,7 +3,7 @@ input=0
 while (($input < 4));
 do
 echo "Choose the operation you want to perform: "
-echo -e "\n Press 1 for Automated Backups \n Press 2 for Disk Space \n Press 3 for Encrypting Disks
+echo -e "\n Press 1 for Automated Backups \n Press 2 for Disk Space \n Press 3 for Encrypting Disks"
 \n Press 4 for exit \n
 read input
 if [ $input -eq 1 ]
